7f0e24aa5a2cdbaa21a9e59ba73923fbb6146aa4
yooodleee <accia25@naver.com>
2025-01-06 19:41:59 +0900

    update springboot study

--- stringboot_study/workspace-spring-tool-suite-4-4.27.0.RELEASE/sbb/src/main/java/com/mysite/sbb/HelloLombok.java
+++ stringboot_study/workspace-spring-tool-suite-4-4.27.0.RELEASE/sbb/src/main/java/com/mysite/sbb/HelloLombok.java
@@ -1,19 +1,16 @@
 package com.mysite.sbb;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Getter
-@Setter
 public class HelloLombok {
-	private String Hello;
-	private int lombok;
+	private final String hello;
+	private final int lombok;
 	
 	public static void main(String[] args) {
-		HelloLombok.helloLombok = new HelloLombok();
-		helloLombok.setHello("헬로");
-		helloLombok.setLombok(5);
-		
+		HelloLombok helloLombok = new HelloLombok("헬로", 5);
 		System.out.println(helloLombok.getHello());
 		System.out.println(helloLombok.getLombok());
 	}
