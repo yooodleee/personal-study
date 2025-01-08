c2f38b93face9888decd8a39d471582d26222521
yooodleee <accia25@naver.com>
2025-01-08 19:37:52 +0900

    fix allowed hosts

--- OPJP_PROJECT_DJANGO/OPJP_Django/OPJP_Django/settings.py
+++ OPJP_PROJECT_DJANGO/OPJP_Django/OPJP_Django/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
