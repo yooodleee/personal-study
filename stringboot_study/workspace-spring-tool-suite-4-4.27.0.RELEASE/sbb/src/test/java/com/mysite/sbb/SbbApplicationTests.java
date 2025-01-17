bfb9ccc73f827209aa52487d8b3a9c80e5da3223
yooodleee <accia25@naver.com>
2025-01-17 12:45:40 +0900

    update springboot study

--- stringboot_study/workspace-spring-tool-suite-4-4.27.0.RELEASE/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ stringboot_study/workspace-spring-tool-suite-4-4.27.0.RELEASE/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,25 +1,37 @@
 package com.mysite.sbb;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
-
-import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+
+
 
 @SpringBootTest
 class SbbApplicationTests {
-	
 	@Autowired
 	private QuestionRepository questionRepository;
 	
+	@Transactional
 	@Test
 	void testJpa() {
-		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-		Question q = qList.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		
+		List<Answer> answerList = q.getAnswerList();
+		
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 		}
 	}
