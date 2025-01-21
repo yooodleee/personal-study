22ab82fc7f63407e71dc0f454a83281e26220c8d
yooodleee <accia25@naver.com>
2025-01-21 17:12:17 +0900

    update springboot study

--- stringboot_study/workspace-spring-tool-suite-4-4.27.0.RELEASE/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ stringboot_study/workspace-spring-tool-suite-4-4.27.0.RELEASE/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,5 +21,12 @@ public class QuestionController {
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question list";
+	}
+	
+	@GetMapping(value = "/question/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail";
 	}
 }
