22f5bc4f2fa685966b39bea68f0bde7033dbad7f
yooodleee <accia25@naver.com>
2025-02-04 18:21:36 +0900

    only code

---
+++ Only_Cpp_Code/ch02/ch02_bitwise_OR.cpp
@@ -0,0 +1,25 @@
+// 비트 OR 연산하기
+
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main() {
+
+	int a = 13;
+	int b = 27;
+	int c = a | b;	// 비트 OR 연산
+
+	cout << "a = " << bitset<8>(a) << " : " << a << endl;
+	cout << "b = " << bitset<8>(b) << " : " << b << endl;
+	cout << "c = " << bitset<8>(c) << " : " << c << endl;
+
+	return 0;
+}
+
+/*
+a = 00001101 : 13
+b = 00011011 : 27
+c = 00011111 : 31
+*/
