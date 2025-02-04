f76516e538c6f1af7fc2dddf4aa58c85dd6bfb6c
yooodleee <accia25@naver.com>
2025-02-04 17:20:59 +0900

    code

---
+++ DoItCpp/ConsoleApplication1/ConsoleApplication1/ch02_localVariable.cpp
@@ -0,0 +1,30 @@
+// 지역 범위가 적용된 변수
+
+#include <iostream>
+using namespace std;
+
+void print() {
+	// 함수 내부의 지역 변수
+	int value = 10;
+	cout << "print 함수 내부에서의 지역 변수 value: " << value << endl;
+}
+
+int main() {
+	// main 함수 내부이 지역 변수
+	int value = 5;
+	cout << "main 함수 내부에서의 지역 분수 value: " << value << endl;
+
+	
+	// print 함수 호출
+	print();
+	// print 함수 호출 후에도 main 함수의 value에 영향을 주지 않음
+	cout << "다시 main 함수 내부에서의 지역 변수 value: " << value << endl;
+
+	return 0;
+}
+
+/*
+main 함수 내부에서의 지역 분수 value: 5
+print 함수 내부에서의 지역 변수 value: 10
+다시 main 함수 내부에서의 지역 변수 value: 5
+*/
