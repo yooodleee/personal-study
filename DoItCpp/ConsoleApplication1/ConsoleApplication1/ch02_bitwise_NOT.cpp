f848100e9a0d2370c63e09d1311a6d5e88943206
yooodleee <accia25@naver.com>
2025-02-04 18:13:33 +0900

    code

---
+++ DoItCpp/ConsoleApplication1/ConsoleApplication1/ch02_bitwise_NOT.cpp
@@ -0,0 +1,14 @@
+// 비트 연산자로 1의 보수 구하기
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	unsigned int value = 0x00000000;	// 0을 16진수(hex)로 표현한 값
+
+	value = ~value;
+	cout << hex << value << endl;
+
+	return 0;
+}
