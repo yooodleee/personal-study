23a5cea0e576ddd21bce97e6a6d69112881cc4ab
yooodleee <accia25@naver.com>
2025-02-05 11:19:08 +0900

    only code

---
+++ Only_Cpp_Code/ch03/ch03_new_delete_array.cpp
@@ -0,0 +1,23 @@
+// new, delete로 배열 형태의 동적 메모리 할당하고 해제하기
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int* pt_int_array_value = new int[5];	// 동적 메모리 할당(배열)
+
+	for (int i = 0; i < 5; i++)
+	{
+		pt_int_array_value[i] = i;	// 할당된 배열 변수에 0~4까지 순서대로 넣기
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << pt_int_array_value[i] << endl;	// 배열 변수 출력
+	}
+
+	delete[] pt_int_array_value;	// 동적 메모리 해제(배열)
+
+	return 0;
+}
