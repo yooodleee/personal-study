70270b578a877ae28ad00d4bca26b8aab7a693cf
yooodleee <accia25@naver.com>
2025-02-05 20:46:37 +0900

    code

---
+++ DoItCpp/ConsoleApplication1/ConsoleApplication1/ch03_pointer_variable_to_integer.cpp
@@ -0,0 +1,22 @@
+// 포인터 변수 자체를 상수화
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a = 0;
+	int b = 1;
+	int* const ptr = &a;	// ptr을 상수화
+
+	a = 1;	// 컴파일 통과
+	ptr = &b;	// 컴파일 오류 발생
+
+	return 0;
+}
+
+/*
+심각도	코드	설명	프로젝트	파일	줄	비표시 오류(Suppression) 상태	세부 정보
+오류(활성)	E0137	식이 수정할 수 있는 lvalue여야 합니다.	
+*/
