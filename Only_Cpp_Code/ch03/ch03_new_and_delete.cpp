af30424d1dce73dd15b5853b88610a728443be0c
yooodleee <accia25@naver.com>
2025-02-05 11:13:49 +0900

    only code

---
+++ Only_Cpp_Code/ch03/ch03_new_and_delete.cpp
@@ -0,0 +1,16 @@
+// new, delete로 동적 메모리 할당하고 해제하기
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int* pt_int_value = new int;	// 동적 메모리 할당
+
+	*pt_int_value = 100;
+	cout << *pt_int_value << endl;
+
+	delete pt_int_value;	// 동적 메모리 해제
+
+	return 0;
+}
