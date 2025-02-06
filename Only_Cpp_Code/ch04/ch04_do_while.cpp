3ea47a44dc7218a3d255fd51cbbf41a6ea6110f3
yooodleee <accia25@naver.com>
2025-02-06 09:33:50 +0900

    only code

---
+++ Only_Cpp_Code/ch04/ch04_do_while.cpp
@@ -0,0 +1,25 @@
+// do ~ while ¹Ýº¹¹®
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+	int count = 0;
+
+	do {
+		cout << count << endl;
+		count++;
+	} while (count < 5);
+
+	return 0;
+}
+
+/*
+0
+1
+2
+3
+4
+*/
