8f93e627ee3355d4387526058b956efbec173344
yooodleee <accia25@naver.com>
2025-02-06 09:19:38 +0900

    only code

---
+++ Only_Cpp_Code/ch04/ch04_if_2.cpp
@@ -0,0 +1,27 @@
+// if를 활용한 조건 분기(중괄호 생략)
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+	int input_number;
+
+	cout << "정수 입력 : ";
+	cin >> input_number;
+
+	if (input_number > 0)
+		cout << "입력한 수는 양수 입니다." << endl;
+	else if (input_number < 0)
+		cout << "입력한 수는 음수 입니다." << endl;
+	else
+		cout << "입력한 수는 0 입니다." << endl;
+
+	return 0;
+}
+
+/*
+정수 입력 : -1
+입력한 수는 음수 입니다.
+*/
