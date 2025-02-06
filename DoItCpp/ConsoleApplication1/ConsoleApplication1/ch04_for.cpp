11fcffaf7084dace32ae7a82f96aef3126524942
yooodleee <accia25@naver.com>
2025-02-06 09:39:29 +0900

    code

---
+++ DoItCpp/ConsoleApplication1/ConsoleApplication1/ch04_for.cpp
@@ -0,0 +1,22 @@
+// for ¹Ýº¹¹®
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+	for (int count = 0; count < 5; count++) {
+		cout << "count : " << count << endl;
+	}
+
+	return 0;
+}
+
+/*
+count : 0
+count : 1
+count : 2
+count : 3
+count : 4
+*/
