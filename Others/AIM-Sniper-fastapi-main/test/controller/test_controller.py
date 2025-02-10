3148569d2256e69728f0f229495eab7cbed0cb23
yooodleee <accia25@naver.com>
2025-02-10 13:03:58 +0900

    for study

--- Others/AIM-Sniper-fastapi-main/test/controller/test_controller.py
+++ Others/AIM-Sniper-fastapi-main/test/controller/test_controller.py
@@ -8,14 +8,17 @@ from fastapi.responses import JSONResponse
 
 from test.service.test_service_impl import TestServiceImpl
 from user_defined_queue.repository.user_defined_queue_repository_impl import UserDefinedQueueRepositoryImpl
-from template.include.socket_server.utility.color_print import ColorPrinter
+from template.include.socket_server.utility.color_print import ColorPrinter # type: ignore
 
 
+# .., template 디렉터리에서 필요한 기능 가져옴.
 sys.path.append(os.path.join(
     os.path.dirname(__file__), 
     '..', 
     'template')
 )
+
+# .., template, include, socket_server 디렉터리에서 필요한 기능을 가져옴.
 sys.path.append(os.path.join(
     os.path.dirname(__file__), 
     '..', 
@@ -30,17 +33,37 @@ testRouter = APIRouter()
 
 
 def injectTestService() -> TestServiceImpl:
+    """
+    Return
+    ---------------
+        TestServiceImpl에 UserDefinedQueueRepository의 객체 정보를 가져와 저장함.
+    """
 
     return TestServiceImpl(
         UserDefinedQueueRepositoryImpl.getInstance()
     )
 
 
-
+# 데코레이터(decorator): 함수에 추가하고 싶은 내용이나 기능
+# testRouter에서 /test-result를 가져와서 requestOpenaiApiResult에 추가함.
 @testRouter.get('/test-result')
 def requestOpenaiApiResult(
     testService: TestServiceImpl = Depends(injectTestService)
 ):
+    """
+    Params
+    -------------
+        testService: 
+            TestServiceImpl에서 injectTestService에 종속된 정보를 가져옴.
+        testResult:
+            testService에서 요청한 test result를 가져옴.
+
+    Return
+    -------------
+        JSONResponse:
+            content: testResult
+            status_code: HTTP_200_OK(정상 실행)
+    """
 
     ColorPrinter.print_important_message("requestTestResult()")
 
