09eb1cf4869b2eca30e8e8ea1161630472725411
yooodleee <accia25@naver.com>
2025-02-10 17:41:02 +0900

    update

--- Others/AIM-Sniper-fastapi-main/openai_api/service/openai_api_service_impl.py
+++ Others/AIM-Sniper-fastapi-main/openai_api/service/openai_api_service_impl.py
@@ -8,7 +8,7 @@ from user_defined_queue.repository.user_defined_queue_repository_impl import Use
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'template'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'template', 'include', 'socket_server'))
 
-from template.include.socket_server.utility.color_print import ColorPrinter
+from template.template.include.socket_server.socket_server.utility.color_print import ColorPrinter
 
 class OpenaiApiServiceImpl(OpenaiApiService):
     def __init__(self, userDefinedQueueRepository: UserDefinedQueueRepositoryImpl):
