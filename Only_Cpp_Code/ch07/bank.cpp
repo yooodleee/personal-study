beb8bff54576e08ff056a448c13995c707771710
yooodleee <accia25@naver.com>
2025-02-17 09:17:34 +0900

    code

--- Only_Cpp_Code/ch07/bank.cpp
+++ Only_Cpp_Code/ch07/bank.cpp
@@ -33,7 +33,8 @@ void bank::use_counter(int _in, int _out) {
 }
 
 int main() {
-    bank my_bank;   // my_bank 인스턴스 생성
+    bank my_bank;   // my_bank 인스턴스 생성(클래스가 메모리에 할당되어 생성된 변수=객체object)
+    // bank 클래스의 멤버 함수인 use_counter를 호출할 수 있음.
 
     my_bank.use_counter(0, 20); // 출금 20
     my_bank.use_counter(50, 0); // 입금 50
@@ -41,3 +42,11 @@ int main() {
 
     return 0;
 }
+
+/*
+용어 정리
+
+* 객체(object): 논리적 개념으로 클래스로 구현하고자 하는 구현 대상, 또는 인스턴스를 달리 부를 때 사용
+* 클래스(class): 객체를 구현하기 위한 설계도
+* 인스턴스(instance): 클래스 정의에 따라 메모리에 실체화된 객체의 형태태
+*/
