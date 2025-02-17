da2bd19c21c4ff0ba630e334e5bcd6a2277c339c
yooodleee <accia25@naver.com>
2025-02-17 10:37:05 +0900

    code

---
+++ Only_Cpp_Code/ch07/initialization_constructor.cpp
@@ -0,0 +1,68 @@
+// 생성자 직접 호출하기
+
+#include <iostream>
+
+using namespace std;
+
+
+// 캐릭터 클래스
+class character {
+public:
+    character() : hp(100), power(100) {};
+
+
+protected:
+    int hp;
+    int power;
+
+};
+
+
+// 플레이어 클래스
+class player : public character {
+public:
+    player();
+
+};
+
+
+// 기본 몬스터 클래스
+class monster {
+public:
+    monster();
+    void get_damage(int _damage);
+    void attack(player target_player) {};
+    void attack_special(player target_player) {};
+
+};
+
+void monster::attack_special(player target_player) {
+    cout << "기본 공격 : 데미지 - 10 hp" << endl;
+}
+
+
+class monster_a : public monster, character {
+public:
+    monster_a() : monster_a(10, 10) {   // 초기화 목록으로 다른 생성자 호출
+        cout << "monster_a 클래스 생성자" << endl;
+    };
+
+    monster_a(int x, int y) : location{x, y} {
+        cout << "monster_a 클래스 생성자 (매개변수 추가)" << endl;
+    };
+
+    void show_location() {
+        cout << "위치(" << location[0] << " , " << location[1] << ")" << endl;
+    };
+
+private:
+    int location[2];
+};
+
+
+int main() {
+    monster_a forest_monster;
+    forest_monster.show_location();
+
+    return 0;
+}
