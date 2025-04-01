cec399f6f36d0ce6d709aab913a90cb46efdb164
yooodleee <accia25@naver.com>
2025-04-01 15:19:14 +0900

    code

--- Only_Cpp_Code/ch07/basic_destructor.cpp
+++ Only_Cpp_Code/ch07/basic_destructor.cpp
@@ -1,5 +1,24 @@
 // 소멸자 정의
 
+/**
+ * 소멸자
+ * 
+ * 소멸자(destructor)는 생성자와 반대 역할을 한다. 생성자에서 객체가 생성될 때 필요한 여러 가지 일을
+ * 진행했다면, 소멸자에서는 객체가 소멸할 때 필요한 메모리 해제나 외부 환경을 객체 생성 이전 상태로
+ * 변경하는 등의 일을 진행한다. 
+ * 
+ * 기본 소멸자
+ * 
+ * 소멸자를 선언하는 방법은 간단하다. 이름은 클래스 이름과 같게 작성하고 이름 앞에 ~ 를 추가한다. 다른
+ * 함수와는 다르게 반환 형식이나 매개변수는 없다. 
+ * 소멸자 선언 
+ * => ~클래스_이름()
+ * 
+ * 소멸자는 객체가 소멸되는 시점에 자동으로 호출되낟. 소멸자는 생성자보다 간단하다. 주로 메모리나 정적 
+ * 변숫값을 생성 전으로 되돌리는 작업이 많아서 매개변수가 필요하지 않다. 그리고 다중 상속일 때 생성자
+ * 호출의 역순으로 소멸자가 호출된다. 
+ */
+
 #include <iostream>
 
 using namespace std;
@@ -8,7 +27,8 @@ using namespace std;
 
 class character {
 public:
-    character() {
+    character() 
+    {
         cout << "character 클래스 생성자" << endl;
     };
 
@@ -19,33 +39,51 @@ public:
 
 
 
-class monster {
+class monster 
+{
 public:
-    monster() {
+    monster() 
+    {
         cout << "monster 클래스 생성자" << endl;
     };
 
-    ~monster() {    // monster의 소멸자 정의
+    ~monster() 
+    {    // monster의 소멸자 정의
         cout << "monster 클래스 소멸자" << endl;
     }
 };
 
 
 
-class monster_a : public monster, character {
+class monster_a : public monster, character 
+{
 public:
-    monster_a() {
+    monster_a() 
+    {
         cout << "monster_a 클래스 생성자" << endl;
     };
 
-    ~monster_a() {  // monster_a의 소멸자 정의
+    ~monster_a() 
+    {  // monster_a의 소멸자 정의
         cout << "monster_a 클래스 소멸자" << endl;
     }
 };
 
 
 
-int main() {
+int main() 
+{
     monster_a forest_monster;
     return 0;
 }
+
+/**
+ * 실행 결과
+ * 
+ * monster 클래스 생성자
+ * character 클래스 생성자
+ * monster_a 클래스 생성자
+ * monster_a 클래스 생성자
+ * character 클래스 생성자
+ * monster 클래스 생성자
+ */
