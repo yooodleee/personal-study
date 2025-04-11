a199b66010545a5b496ee6702351e237a77ae0b4
yooodleee <accia25@naver.com>
2025-04-11 09:33:44 +0900

    code

--- Only_Cpp_Code/ch08/mon_function_override.cpp
+++ Only_Cpp_Code/ch08/mon_function_override.cpp
@@ -1,12 +1,18 @@
 // 일반 함수를 활용한 오버라이딩 호출 변화
 
+/**
+ * 가상 함수를 사용하지 않고 오버라이딩했을 때의 동작도 살펴보도록 하자. 이전 코드와는 다르게 
+ * 업캐스팅 후에 부모의 멤버 함수가 호출되는 것을 확인할 수 있다. 
+ */
+
 #include <iostream>
 
 using namespace std;
 
 
 
-class character {
+class character 
+{
 public:
     character() : hp(100), power(100) {};
 
@@ -18,7 +24,8 @@ protected:
 
 
 // character 클래스를 상속받은 player 클래스
-class player : public character {
+class player : public character 
+{
 public:
     player() {};
 };
@@ -26,7 +33,8 @@ public:
 
 
 // 기본 Monster클래스
-class monster {
+class monster 
+{
 public:
     monster() {};
     void get_damage(int _damage) {};
@@ -34,49 +42,57 @@ public:
     void attack_special(player target_player) {};
 };
 
-void monster::attack_special(player target_player) {
+void monster::attack_special(player target_player) 
+{
     cout << "기본 공격 : 데미지 - 10 hp" << endl;
 }
 
 
 // 몬스터 A는 기본 Monster 클래스로부터 상속
-class monster_a : public monster, character {
+class monster_a : public monster, character 
+{
 public:
     // 상속받은 함수 오버라이딩
     void attack_special(player target_player);
 };
 
-void monster_a::attack_special(player target_player) {
+void monster_a::attack_special(player target_player) 
+{
     cout << "인텡글 공격 : 데미지 - 15 hp" << endl;
 }
 
 
 // 몬스터 B는 기본 Monster 클래스로부터 상속
-class monster_b : public monster, character {
+class monster_b : public monster, character 
+{
 public:
     // 상속받은 함수 오버라이딩
     void attack_special(player target_player);
 };
 
-void monster_b::attack_special(player target_player) {
+void monster_b::attack_special(player target_player) 
+{
     cout << "가상 공격 : 데미지 - 0 hp" << endl;
 }
 
 
 // 몬스터 C는 기본 Monster 클래스로부터 상속
-class monster_c : public monster, character {
+class monster_c : public monster, character 
+{
 public:
     // 상속받은 함수 오버라이딩
     void attack_special(player target_player);
 };
 
-void monster_c::attack_special(player target_player) {
+void monster_c::attack_special(player target_player) 
+{
     cout << "강력 뇌전 공격 : 데미지 - 100 hp" << endl;
 }
 
 
 
-int main() {
+int main() 
+{
     player player_1;
 
     monster_a forest_monster;
@@ -92,3 +108,14 @@ int main() {
 
     return 0;
 }
+
+/**
+ * 실행 결과
+ * 
+ * 부모 클래스로 업캐스팅 후 공격
+ * 기본 공격 : 데미지 - 10 hp
+ * 
+ * 자식 클래스로 공격
+ * 인텡글 공격 : 데미지 - 15 hp
+ */
+
