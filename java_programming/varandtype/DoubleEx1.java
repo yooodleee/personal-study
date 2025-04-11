2dbfbfb5b7a079786540e6d30af633be6b037170
yooodleee <accia25@naver.com>
2025-04-11 15:25:34 +0900

    Double

---
+++ java_programming/varandtype/DoubleEx1.java
@@ -0,0 +1,11 @@
+package varandtype;
+
+public class DoubleEx1 {
+    public static void main(String[] args) {
+        double dnum = 3.14;
+        float fnum = 3.14F; // F: 식별자(identificator)
+
+        System.out.println(dnum);
+        System.out.println(fnum);
+    }
+}
