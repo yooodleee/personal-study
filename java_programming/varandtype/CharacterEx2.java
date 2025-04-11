0c8a3f6360de1cee7a83e00a5a498161d76ddcc4
yooodleee <accia25@naver.com>
2025-04-11 15:05:06 +0900

    character

---
+++ java_programming/varandtype/CharacterEx2.java
@@ -0,0 +1,11 @@
+package varandtype;
+
+public class CharacterEx2 {
+    public static void main(String[] args) {
+        char ch1 = '한';
+        char ch2 = '\uD55C';
+
+        System.out.println(ch1);    // 한
+        System.out.println(ch2);    // 한
+    }
+}
