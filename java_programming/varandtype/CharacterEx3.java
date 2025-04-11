d4bc2bd47e9a40cf9beb8a7480f317989486bbf8
yooodleee <accia25@naver.com>
2025-04-11 15:07:03 +0900

    character

---
+++ java_programming/varandtype/CharacterEx3.java
@@ -0,0 +1,15 @@
+package varandtype;
+
+public class CharacterEx3 {
+    public static void main(String[] args) {
+        int a = 65;
+        int b = -66;
+
+        char a2 = 65;
+        // char b2 = -66;       => 문자형 변수에 음수를 넣으면 오류 발생
+
+        System.out.println((char)a);
+        System.out.println((char)b);
+        System.out.println(a2);
+    }
+}
