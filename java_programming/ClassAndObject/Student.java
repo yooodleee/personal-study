cb31ca18ec1a3d7c96cfc13c04e97d336d97ec9d
yooodleee <accia25@naver.com>
2025-04-23 21:04:01 +0900

    code

--- java_programming/ClassAndObject/Student.java
+++ java_programming/ClassAndObject/Student.java
@@ -6,4 +6,8 @@ public class Student {
     String studentName;     // 이름
     int grade;              // 학년
     String address;         // 주소
+
+    public void showStudentInfo() {
+        System.out.println(studentName + ", " address); // 이름, 주소 출력
+    }
 }
