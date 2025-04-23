1ae44b918378af917c90c43c977b081329311d5d
yooodleee <accia25@naver.com>
2025-04-23 20:59:26 +0900

    code

---
+++ java_programming/ClassAndObject/Student.java
@@ -0,0 +1,9 @@
+package ClassAndObject;
+
+public class Student {
+    // 멤버 변수
+    int studentId;          // 학번
+    String studentName;     // 이름
+    int grade;              // 학년
+    String address;         // 주소
+}
