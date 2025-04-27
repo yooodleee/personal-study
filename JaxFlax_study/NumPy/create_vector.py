cfe4100fbe22c3463e59163586f644a72a547365
yooodleee <accia25@naver.com>
2025-04-27 22:43:55 +0900

    code

---
+++ JaxFlax_study/NumPy/create_vector.py
@@ -0,0 +1,11 @@
+import jax
+import jax.numpy as jnp
+import numpy as np
+from IPython.display import display
+
+
+x = np.array([1.0, 2.0, 3.0, 4.0])
+y = np.array([5.0, 6.0, 7.0, 8.0])
+z = x + y
+
+display(z)
