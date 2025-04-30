877c2c539bafc73e6e3551004f846f06717ff606
yooodleee <accia25@naver.com>
2025-04-30 22:32:37 +0900

    code

--- Only_Cpp_Code/ch08/virtual_destructor_second.cpp
+++ Only_Cpp_Code/ch08/virtual_destructor_second.cpp
@@ -6,17 +6,19 @@ using namespace std;
 
 
 
-class monster {
+class monster 
+{
 public:
     monster();
     // ~monster();
-    virtual ~monster();
+    virtual ~monster(); // 가상 소멸자 사용 
 
 private:
     int* dummy;
 };
 
-monster::monster() {
+monster::monster() 
+{
     cout << "monster() 생성자 호출" << endl;
     dummy = new int;
 }
@@ -27,7 +29,8 @@ monster::~monster() {
 }
 
 
-class monster_a : public monster {
+class monster_a : public monster 
+{
 public:
     monster_a();
     // ~monster_a();
@@ -37,18 +40,21 @@ private:
     int* dummy_a;
 };
 
-monster_a::monster_a() {
+monster_a::monster_a() 
+{
     cout << "monster_a() 생성자 호출" << endl;
     dummy_a = new int;
 }
 
-monster_a::~monster_a() {
+monster_a::~monster_a() 
+{
     cout << "~monster_a() 소멸자 호출" << endl;
     delete dummy_a;
 }
 
 
-int main() {
+int main() 
+{
     monster* mon = new monster_a();     // 부모 클래스로 업캐스팅
     delete mon;
     
