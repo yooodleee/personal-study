1c0c5ad8e580254a1ca5584bc4438c49a1741c4f
yooodleee <accia25@naver.com>
2025-05-16 23:03:32 +0900

    code

---
+++ java_study/untitled/src/loopexample/DoWhileExample.java
@@ -0,0 +1,15 @@
+package loopexample;
+
+public class DoWhileExample {
+    public static void main(String[] args) {
+        int num = 1;
+        int sum = 0;
+
+        do {
+            sum += num;
+            num++;
+        } while (num <= 10);    // 조건식이 참이 아니더라도 무조건 참 실행
+
+        System.out.println("1부터 10까지의 합은 " + sum + "입니다.");
+    }
+}
--- java_study/untitled/src/loopexample/WhileExample1.java
+++ java_study/untitled/src/loopexample/WhileExample1.java
@@ -5,7 +5,7 @@ public class WhileExample1 {
         int num = 1;
         int sum = 0;
 
-        while (num <= 10) { // 조건식이 참일 경우에만 실행 
+        while (num <= 10) { // 조건식이 참일 경우에만 실행
             // num 값이 10보다 작거나 같을 동안
             sum += num; // 합계를 뜻하는 sum에 num을 더하고
             num++;      // num에 1씩 더해 나감
