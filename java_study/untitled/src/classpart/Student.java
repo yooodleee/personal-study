aead0c802f15e0648736856eabe5e6c8f2d9357c
yooodleee <accia25@naver.com>
2025-05-16 23:13:24 +0900

    code

---
+++ java_study/untitled/src/classpart/Student.java
@@ -0,0 +1,8 @@
+package classpart;
+
+public class Student {
+    int studentId;          // 학번
+    String studentName;     // 학생 이름
+    int grade;              // 학년
+    String address;         // 사는 곳
+}
