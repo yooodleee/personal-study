99904945b973262c0ee9178ee7090b1819cb544b
yooodleee <accia25@naver.com>
2025-05-16 23:25:19 +0900

    code

---
+++ java_study/untitled/src/classpart/StudentTest.java
@@ -0,0 +1,11 @@
+package classpart;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student studentAhn  = new Student();    // Student 클래스 생성
+        StudentAhn.studentName = "안연수";
+
+        System.out.println(studentAhn.studentName);
+        System.out.println(studentAhn.getStudentName());
+    }
+}
