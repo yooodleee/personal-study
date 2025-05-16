d8d8417f39e254bf991e793feaeb2f839ca90f3c
yooodleee <accia25@naver.com>
2025-05-16 23:22:39 +0900

    code

--- java_study/untitled/src/classpart/Student.java
+++ java_study/untitled/src/classpart/Student.java
@@ -17,4 +17,12 @@ public class Student {
     public void setStudentName(String name) {
         studentName = name;
     }
+
+    public static void main(String[] args) {
+        Student studentAhn = new Student(); // Student 클래스 생성
+        studentAhn.studentName = "안연수";
+
+        System.out.println(studentAhn.studentName);
+        System.out.println(studentAhn.getStudentName());
+    }
 }
