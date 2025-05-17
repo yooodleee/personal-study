e12a1c5be91b535c6cebb84f2c8d6eef7d2e45e7
yooodleee <accia25@naver.com>
2025-05-17 23:39:53 +0900

    code

--- java_study/untitled/src/constructor/Person.java
+++ java_study/untitled/src/constructor/Person.java
@@ -4,4 +4,10 @@ public class Person {
     String name;
     float height;
     float weight;
+
+    public Person() { } // 디폴트 생성자 추가 
+
+    public Person(String pname) {
+        name = pname;   // 사람 이름을 매개변수로 입력 받아서 Person 클래스를 생성하는 생성자 구현
+    }
 }
