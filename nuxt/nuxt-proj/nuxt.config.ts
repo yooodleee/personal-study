7c7494d9e59ee3abc40960d71d80a600ad096db1
yooodleee <accia25@naver.com>
2025-05-17 23:12:21 +0900

    prepare

--- nuxt/nuxt-proj/nuxt.config.ts
+++ nuxt/nuxt-proj/nuxt.config.ts
@@ -1,6 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+import { defineNuxtConfig } from "nuxt/config"
+
+export default defineNuxtConfig ({
+  compatibilityDate: 'latest',
   devtools: { enabled: true },
+
+  css: [
+    'vuetify/styles',
+    '@mdi/font/css/materialdesignicons.min.css',
+  ],
+
+  build: {
+    transpile: ['vuetify']
+  },
+
+  vite: {
+    ssr: {
+      noExternal: ['vuetify'],
+    },
+  },
+
   modules: ['vuetify-nuxt-module']
 })
