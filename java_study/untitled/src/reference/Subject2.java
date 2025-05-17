0b3f25662461bfe0c68793597253d9b125f8654b
yooodleee <accia25@naver.com>
2025-05-17 23:51:13 +0900

    code

---
+++ java_study/untitled/src/reference/Subject2.java
@@ -0,0 +1,23 @@
+package reference;
+
+public class Subject2 {
+
+    private String subjectName;
+    private int scorePoint;
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public int getScorePoint() {
+        return scorePoint;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public void setScorePoint(int scorePoint) {
+        this.scorePoint = scorePoint;
+    }
+}
