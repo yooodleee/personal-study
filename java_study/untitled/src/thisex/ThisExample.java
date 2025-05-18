b6e0cfbb688ec946da56067aad15abbdc58010b1
yooodleee <accia25@naver.com>
2025-05-18 23:22:49 +0900

    code

---
+++ java_study/untitled/src/thisex/ThisExample.java
@@ -0,0 +1,25 @@
+package thisex;
+
+
+class BirthDay {
+    int day;
+    int month;
+    int year;
+}
+
+public void setYear(int year) {
+    this.year = year;
+}
+
+public void printThis() {
+    System.out.println(this);       // this 출력 메서드
+}
+
+public class ThisExample {
+    public static void main(String[] args) {
+        BirthDay bDay = new BirthDay();
+        bDay.setYear(2000);         // 태어난 연도를 2000으로 지종
+        System.out.println(bDay);   // 참조 변수 출력
+        bDay.printThis();           // this 출력 메서드 호출
+    }
+}
