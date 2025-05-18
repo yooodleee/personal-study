12d93341b67a048066a4c5ee69fdb876d691f4e2
yooodleee <accia25@naver.com>
2025-05-18 23:08:43 +0900

    code

--- java_study/untitled/src/hiding/StudentTest.java
+++ java_study/untitled/src/hiding/StudentTest.java
@@ -6,6 +6,6 @@ public class StudentTest {
 //        studentLee.studentName = "이상원";         // 오류 발생
         studentLee.setStudentName("이상원");         // setStudentName() 메서드를 활용해 private 변수에 접근 가능
 
-        System.out.println(studentLee.getStudentName());    
+        System.out.println(studentLee.getStudentName());
     }
 }
